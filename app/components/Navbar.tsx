a3457919e9a816febee01a920887a3bb11464e6a
Sandeep-2412 <machasndp@gmail.com>
2026-01-26 22:58:58 -0500

    Resume Feedback Page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <Link to="/">
         <p className="text-2xl font-bold text-gradient">RESUMIND</p>
       </Link>
-      <Link to="/uupload">
+      <Link to="/upload">
         <p className="primary-button w-fit">Upload Resume</p>
       </Link>
     </nav>
